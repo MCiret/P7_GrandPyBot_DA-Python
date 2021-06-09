f71c63cb310f8901c72b4007361e96ebf133e0d1
MaCi <ciret.m@gmail.com>
2021-06-09 15:52:14 +0200

    modif main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(port=os.environ['PORT'], debug=True)
+    # for key in os.environ:
+    #     print(key, "---->", os.environ[key])
+    # print(os.environ['PORT'])
+    app.run(debug=True)
