f2819c6525dc58479a5dbc537e60fd33b01f48cf
marie <ciret.m@gmail.com>
2024-05-10 20:16:36 +0200

    Try logging lib

--- app/views.py
+++ app/views.py
@@ -2,9 +2,14 @@ from flask import render_template, request
 from . import app
 import app.grandpy_bot as gpy
 
+import logging
+import os
+
 
 @app.route("/")
 def home():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(os.environ)
     return render_template("index.html")
 
 
