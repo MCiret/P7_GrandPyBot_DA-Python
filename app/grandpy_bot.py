57e9ea22fe1be756eb369d3f5402ec38a5b7e365
marie <ciret.m@gmail.com>
2024-05-10 21:05:24 +0200

    Fix production PIPENV_DOTENV_LOCATION error --> add it in supervisor conf file

--- app/grandpy_bot.py
+++ app/grandpy_bot.py
@@ -2,8 +2,6 @@ import config as cfg
 import app.api_requests as api
 from app.parser import Parser
 
-import logging
-
 
 def parse_question(whole_question: str) -> dict:
     parsed_question = Parser(whole_question)
@@ -20,8 +18,6 @@ def parse_question(whole_question: str) -> dict:
 
 
 def maps_position(location: str) -> dict:
-    logging.basicConfig(level=logging.DEBUG)
-    logging.debug(f"maps_position - cfg.HERE_API_KEY = {cfg.HERE_API_KEY}")
     return api.resp_dict_to_location(
                 api.api_request_and_get_dict_resp(cfg.HERE_API_URL, apiKey=cfg.HERE_API_KEY, q=location))
 
